b557e5b8eb1add73913d1c7e63fa3394780018f8
hquach <hoaquach@gmail.com>
2017-06-10 10:26:29 -0700

    first

---
+++ data.php
@@ -0,0 +1,30 @@
+<?php
+
+$username = "";
+$password = "";
+$host = "";
+$database = "";
+
+$server = mysql_connect($host, $username, $password);
+$connection = mysql_select_db($database, $server);
+
+$myquery = "SELECT * FROM table";
+
+$query = mysql_query($myquery);
+
+if ( ! $query ) {
+	echo mysql_error();
+	die;
+}
+
+$data = array();
+
+for ($x = 0; $x < mysql_num_rows($query); $x++) {
+	$data[] = mysql_fetch_assoc($query);
+}
+
+echo json_encode($data);
+
+mysql_close($server);
+
+?>
